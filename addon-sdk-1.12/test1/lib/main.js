d6071ab4142a7c2fbfc5c35b9591c8b1d6d48001
Priyank Doshi <priyank.doshi.ce@gmail.com>
2012-12-18 02:32:28 -0800

    test1 init

---
+++ addon-sdk-1.12/test1/lib/main.js
@@ -0,0 +1,11 @@
+var l = List.compose(1,2,3);
+exports.main = function(){
+for each(var i in l)
+console.log(i);
+};
+
+var ps = require("preferences-service");
+
+consol.log(ps.get("extensions.checkCompatibility.nightly"));
+ 
+ 
